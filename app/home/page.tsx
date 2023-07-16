47b8592a062642f3ed7612b812f5d97cd4ff17af
soymikey <soymikey0717@gmail.com>
2023-07-16 18:24:57 +0800

    add user logout

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,14 @@
+import UserNav from "@/components/UserNav";
 import React from "react";
 
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div className="flex-1 mt-10 mx-10">
+      <div className="flex justify-end">
+        <UserNav />
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
---
+++ components/UserNav.tsx
@@ -0,0 +1,74 @@
+"use client";
+import React from "react";
+import { DEFAULTAVATAR } from "@/app/constants";
+import { useGlobalContext } from "@/app/context/store";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useRouter } from "next/navigation";
+function UserNav() {
+  const { username, setIsLogin, setUsername } = useGlobalContext();
+  const router = useRouter();
+
+  function logOut() {
+    setIsLogin(false);
+    setUsername("");
+    router.replace("/");
+  }
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <div className="flex items-center">
+          <Button variant="ghost" className="relative h-8 w-8 rounded-full">
+            <Avatar className="h-8 w-8">
+              <AvatarImage src={DEFAULTAVATAR} alt="user-avatar" />
+              <AvatarFallback>Avatar</AvatarFallback>
+            </Avatar>
+          </Button>
+          :{username}
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">{username}</p>
+            <p className="text-xs leading-none text-muted-foreground">
+              {username}@example.com
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            Profile - developing
+            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Billing - developing
+            <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Settings - developing
+            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={logOut}>
+          Log out
+          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+export default UserNav;
